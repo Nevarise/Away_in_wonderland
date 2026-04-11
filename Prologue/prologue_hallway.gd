53d79fdfb5163497770fd75f608406c6f685150f
Nevarise <cqaclan@gmail.com>
2026-04-11 22:45:50 +0800

    Add files via upload
    Overall time: 6.2 hrs -> 6 hrs, 12 minutes
    Last update was 5 hrs, 12 minutes -> worked for an hour
    
    Coded interaction for lamps placed within hallway scene -> when clicked, in turns on/off, simple animation to clock and made the cursor switch whenever hovering over an interactable object
    ->progress slow, still making up the game design, adding more game mechanics (which will mean more editing on player code)

--- Prologue/prologue_hallway.gd
+++ Prologue/prologue_hallway.gd
@@ -1,11 +1,51 @@
 extends Node2D
 
+var interact = preload("res://interact.png")
 
-# Called when the node enters the scene tree for the first time.
+@onready var Light1_ON = $"TileMap/Objects/Light1/Light1-on"
+@onready var Light1_OFF = $"TileMap/Objects/Light1/Light1-off"
+@onready var Light2_ON = $"TileMap/Objects/Light2/Light2-on"
+@onready var Light2_OFF = $"TileMap/Objects/Light2/Light2-off"
+@onready var Light3_ON = $"TileMap/Objects/Light3/Light3-on"
+@onready var Light3_OFF = $"TileMap/Objects/Light3/Light3-off"
+
+
 func _ready() -> void:
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+#Change cursor if on interactable thing
+func interact_cursor():
+	Input.set_custom_mouse_cursor(interact)
+#Change cursor if not on interactable thing
+func normal_cursor():
+	Input.set_custom_mouse_cursor(null)
+
+#Light1
+func _on_light_1_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if Light1_ON.visible: 
+			Light1_ON.visible = false
+			Light1_OFF.visible = true
+		else:
+			Light1_ON.visible = true
+			Light1_OFF.visible = false
+			
+func _on_light_2_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if Light2_ON.visible: 
+			Light2_ON.visible = false
+			Light2_OFF.visible = true
+		else:
+			Light2_ON.visible = true
+			Light2_OFF.visible = false
+
+func _on_light_3_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if Input.is_action_just_pressed("interact"):
+		if Light3_ON.visible: 
+			Light3_ON.visible = false
+			Light3_OFF.visible = true
+		else:
+			Light3_ON.visible = true
+			Light3_OFF.visible = false
